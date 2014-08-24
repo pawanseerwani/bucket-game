8a38c360ff81dc07566c13206d2ab046f6320d04
Pawan Kumar D <pawanseerwani@gmail.com>
2014-08-24 19:49:31 +0530

    Backend part Complete with functional APIs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'game/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'game#index'
+
+   get 'user' => 'game#get_user'
+   post 'update_score' => 'game#update_score'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
